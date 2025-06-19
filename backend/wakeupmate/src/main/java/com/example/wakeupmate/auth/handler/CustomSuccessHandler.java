0b3cbb81dc43fb68f6cc509e6052d512ff32778e
gitFILO <x1xgudwls@naver.com>
2025-06-20 01:42:53 +0900

    fix(be): CORS 처리

--- backend/wakeupmate/src/main/java/com/example/wakeupmate/auth/handler/CustomSuccessHandler.java
+++ backend/wakeupmate/src/main/java/com/example/wakeupmate/auth/handler/CustomSuccessHandler.java
@@ -39,7 +39,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String token = jwtUtil.createJwt(userId, role, 60 * 60 * 60L);
 
         response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect("https://wakeupmate.vercel.app/");
+        response.sendRedirect("https://wakeupmate.my/");
     }
 
     private Cookie createCookie(String key, String value) {
