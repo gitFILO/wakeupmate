93bd4432ad83d145ad62327f6cdb001c604cd6be
gitFILO <x1xgudwls@naver.com>
2025-08-09 16:22:41 +0900

    feat(cache): 캐시 DI - Config 및 구현체 추가

---
+++ backend/wakeupmate/src/main/java/com/example/wakeupmate/common/config/LocationBucketConfig.java
@@ -0,0 +1,2 @@
+package com.example.wakeupmate.common.config;public class LocationBucketConfig {
+}
---
+++ backend/wakeupmate/src/main/java/com/example/wakeupmate/location/cache/impl/BucketsGlobalFlatMap.java
@@ -0,0 +1,2 @@
+package com.example.wakeupmate.location.cache.impl;public class BucketsGlobalFlatMap {
+}
