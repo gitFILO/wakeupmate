19f103d72b210384195f19b43b76413a3f165bac
gitFILO <x1xgudwls@naver.com>
2025-08-10 23:42:19 +0900

    feat(security): X-Header 비허용

--- backend/wakeupmate/src/main/java/com/example/wakeupmate/common/config/SecurityConfig.java
+++ backend/wakeupmate/src/main/java/com/example/wakeupmate/common/config/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
